cfef54f220d5a71e55b7fca358da259424cc24e0
Reetam Roy Burman <contact.iamreetam@gmail.com>
2024-02-17 16:11:03 +0530

    auth controller changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
-const { instructor } = require("../models/Schema");
+const { instructorModel } = require("../models/Schema");
 const bcrypt = require("bcrypt");
+const { uuid } = require('uuidv4');
 const jwt = require("jsonwebtoken");
 const { error, success } = require("../utils/responseWrapper");
 
@@ -13,23 +14,26 @@ const signupController = async (req, res) => {
         }
 
         //if the email already present in database or our Usermodels
-        const oldUser = await instructor.findOne({ email });
+        const oldUser = await instructorModel.findOne({ email });
         if (oldUser) {
             // return res.status(409).send("instructor is already registered");
             return res.send(error(409, "instructor is already registered"));
         }
 
+        const _id = uuid()
+
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const instructor = await instructor.create({
+        const instructor = await instructorModel.create({
             name,
             email,
             password: hashedPassword,
+            user_id:_id
         });
 
-        return res.send(
-            success(201, 'instructor created successfully')
-        );
+        return res.status(200).json({
+            "result": "instructor created successfully"
+        })
     } catch (e) {
         return res.send(error(500, e.message));
     }
@@ -44,7 +48,7 @@ const loginController = async (req, res) => {
             return res.send(error(400, "All fields are required"));
         }
 
-        const instructor = await instructor.findOne({ email }).select('+password');
+        const instructor = await instructorModel.findOne({ email }).select('+password');
         if (!instructor) {
             // return res.status(404).send("instructor is not registered");
             return res.send(error(404, "instructor is not registered"));
@@ -57,18 +61,16 @@ const loginController = async (req, res) => {
         }
 
         const accessToken = generateAccessToken({
-            _id: instructor._id,
-        });
-        const refreshToken = generateRefreshToken({
-            _id: instructor._id,
+            user_id: instructor.user_id,
         });
 
-        res.cookie("jwt", refreshToken, {
-            httpOnly: true,
-            secure: true,
-        });
+        console.log(accessToken);
 
-        return res.send(success(200, { accessToken }));
+        return res.status(200).json({
+            token:accessToken,
+            name :instructor.name,
+            email:instructor.email
+        })
     } catch (e) {
         return res.send(error(500, e.message));
     }
@@ -128,21 +130,9 @@ const generateAccessToken = (data) => {
     }
 };
 
-const generateRefreshToken = (data) => {
-    try {
-        const token = jwt.sign(data, process.env.REFRESH_TOKEN_PRIVATE_KEY, {
-            expiresIn: "1y",
-        });
-        console.log(token);
-        return token;
-    } catch (error) {
-        console.log(error);
-    }
-};
 
 module.exports = {
     signupController,
     loginController,
-    refreshAccessTokenController,
     logoutController
 };
